9f0ce5fbb27a0362fe4dfe9cde4f6963e8179dca
jnjnboo <jen_par@hotmail.com>
2017-11-09 21:10:34 -0800

    Added presentation for the project. Added console logging in debug mode to Web project.

--- ContactBook/ContactBookApi/Startup.cs
+++ ContactBook/ContactBookApi/Startup.cs
@@ -1,14 +1,14 @@
 ﻿using ContactBookApi.Models;
 using ContactBookApi.Repositories;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
-using Microsoft.AspNetCore.Cors.Infrastructure;
 
 namespace ContactBookApi
 {
@@ -25,7 +25,6 @@ namespace ContactBookApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc()
@@ -41,7 +40,6 @@ namespace ContactBookApi
             corsBuilder.AllowAnyHeader();
             corsBuilder.AllowAnyMethod();
             corsBuilder.AllowAnyOrigin();
-            //corsBuilder.WithOrigins("http://localhost:56573"); // for a specific url. Don't add a forward slash on the end!
             corsBuilder.AllowCredentials();
 
             services.AddCors(options =>
@@ -50,20 +48,19 @@ namespace ContactBookApi
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
+                loggerFactory.AddConsole();
             }
 
             app.UseCors("SiteCorsPolicy");
 
-            app.UseMvc(routes =>{
+            app.UseMvc(routes =>
+            {
                 routes.MapRoute(
                     name: "default",
                     template: "v1/{controller=Contacts}/{action=Contact}/{id?}");
--- ContactBook/ContactBookWeb/Startup.cs
+++ ContactBook/ContactBookWeb/Startup.cs
@@ -1,25 +1,23 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-//using System.Management.Automation;
+using Microsoft.Extensions.Logging;
 
 namespace ContactBookWeb
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRouting();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                loggerFactory.AddConsole();
             }
 
             app.UseDefaultFiles();
